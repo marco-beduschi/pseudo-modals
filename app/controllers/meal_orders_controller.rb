1ef03a46dd8018ef479577d9a386143e66ac407b
Marco Beduschi <8469440+marco-beduschi@users.noreply.github.com>
2024-12-09 10:00:05 -0300

    Add base html for MealOrders#show

--- app/controllers/meal_orders_controller.rb
+++ app/controllers/meal_orders_controller.rb
@@ -2,4 +2,18 @@ class MealOrdersController < ApplicationController
   def new
     @meal_order = MealOrder.new
   end
+
+  def create
+    @meal_order = MealOrder.new
+
+    if @meal_order.save
+      redirect_to @meal_order
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @meal_order = MealOrder.find(params[:id])
+  end
 end
