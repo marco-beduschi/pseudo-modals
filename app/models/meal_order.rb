535badb497fea3ddec6bc041290bc8fed986e8a5
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-12-23 16:40:20 -0300

    Add relationship of MealOrder and MealOrderBurger

--- app/models/meal_order.rb
+++ app/models/meal_order.rb
@@ -2,4 +2,5 @@ class MealOrder < ApplicationRecord
   belongs_to :burger, optional: true
   belongs_to :french_fry, optional: true
   belongs_to :drink, optional: true
+  has_one :meal_order_burger
 end
