48aabec6f5560d49d54f773a5674bc3a14a68b37
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-12-23 16:40:05 -0300

    Redirect to MealOrder#show on burger create

--- app/controllers/meal_order_burgers_controller.rb
+++ app/controllers/meal_order_burgers_controller.rb
@@ -5,4 +5,18 @@ class MealOrderBurgersController < ApplicationController
       meal_order: @meal_order
     )
   end
+
+  def create
+    @meal_order = MealOrder.find(params[:meal_order_id])
+    @meal_order_burger = MealOrderBurger.new(
+      meal_order: @meal_order,
+      burger_id: params[:meal_order_burger][:burger_id]
+    )
+
+    if @meal_order_burger.save
+      redirect_to meal_order_path(@meal_order)
+    else
+      render @meal_order_burger
+    end
+  end
 end
